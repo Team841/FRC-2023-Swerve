a40a04cc810a522fce33bb5a838697db55051b75
David Tian <dtian.github@outlook.com>
2023-11-02 22:14:16 -0700

    working swerve drive code

---
+++ src/main/java/com/team841/Swerve23/ControlBoard/xBoxController.java
@@ -0,0 +1,5 @@
+package com.team841.Swerve23.ControlBoard;
+
+public class xBoxController {
+    ;
+}
--- src/main/java/com/team841/offseason2023/Swerve23/Constants/ConstantsIO.java
+++ src/main/java/com/team841/offseason2023/Swerve23/Constants/ConstantsIO.java
@@ -12,6 +12,12 @@ public final class ConstantsIO {
 
   public static boolean inTune = false;
 
+  public static class OI {
+    public static final int driverPortLeft = 0; // controller USB port 0
+    public static final int driverPortRight = 1; // controller USB port 1
+    public static final int codriverPort = 2; // controller USB port 2
+  }
+
   public static final class CANID {
 
     // Swerve falcons
@@ -37,4 +43,6 @@ public final class ConstantsIO {
     // Pigeon
     public static final int kPigeon = 0;
   }
+
+
 }
