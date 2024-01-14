7ebed3ec83fef3b3daa00d99d20550bb733cb354
David Tian <dtian.github@outlook.com>
2024-01-13 21:43:02 -0800

    working LED concept

--- src/main/java/com/team841/Swerve23/Robot.java
+++ src/main/java/com/team841/Swerve23/Robot.java
@@ -2,7 +2,12 @@ package com.team841.Swerve23;
 
 import com.team841.Swerve23.Constants.ConstantsIO;
 import com.team841.Swerve23.Constants.ConstantsIO.robotStates;
+import com.team841.lib.util.tunableNumber;
+
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -11,16 +16,28 @@ public class Robot extends TimedRobot {
 
   private RobotContainer m_robotContainer;
 
+  private Spark LED = new Spark(7);
+
+  private tunableNumber ledValue = new tunableNumber("LED value:" );
+
   @Override
   public void robotInit() {
     m_robotContainer = new RobotContainer();
 
     ConstantsIO.robotCurrentState = robotStates.disabled;
+
+    ledValue.setDefault(0);
+    LED.set(-0.95);
   }
 
   @Override
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
+
+    if (ledValue.hasChanged())
+      LED.set(ledValue.get());
+
+    SmartDashboard.putNumber("led channel", LED.getChannel());
   }
 
   @Override
