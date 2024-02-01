8807165cbf5fafd9675bdba7804f03cf6999b96e
sara-hermosillo <139658030+sara-hermosillo@users.noreply.github.com>
2024-01-31 18:15:57 -0800

    New paths from path planner

--- src/main/java/com/team841/Swerve23/Drive/Drivetrain.java
+++ src/main/java/com/team841/Swerve23/Drive/Drivetrain.java
@@ -1,18 +1,24 @@
 package com.team841.Swerve23.Drive;
 import com.team841.Swerve23.Constants.SC;
+import com.team841.Swerve23.Constants.Swerve;
 
 import com.ctre.phoenix6.mechanisms.swerve.SimSwerveDrivetrain.SimSwerveModule;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrain;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveDrivetrainConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveModuleConstants;
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.ctre.phoenix6.hardware.Pigeon2;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import java.util.function.Supplier;
 
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
+import com.pathplanner.lib.util.PIDConstants;
 import com.pathplanner.lib.util.PathPlannerLogging;
+import com.pathplanner.lib.util.ReplanningConfig;
 import com.team841.Swerve23.Constants.SC;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -22,10 +28,13 @@ import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.Sendable;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.math.geometry.Translation2d;
+
+
 
 public class Drivetrain extends SwerveDrivetrain implements Subsystem {
 
@@ -36,21 +45,27 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
 
     private SimGyro gyro;
     private Field2d field = new Field2d();
+    
+    private final SwerveRequest.ApplyChassisSpeeds autoRequest = new SwerveRequest.ApplyChassisSpeeds();
+    
   public Drivetrain(
       SwerveDrivetrainConstants driveTrainConstants,
       double OdometryUpdateFrequency,
       SwerveModuleConstants... modules) {
     super(driveTrainConstants, OdometryUpdateFrequency, modules);
-    Swerve();
+    
+    configurePathPlanner();
+    //pidgey = new Pigeon2(0);
+
   }
 
   public Drivetrain(
       SwerveDrivetrainConstants driveTrainConstants, SwerveModuleConstants... modules) {
     super(driveTrainConstants, modules);
-    Swerve();
+    configurePathPlanner();
   }
 
- public void Swerve() {
+ public void configurePathPlanner() {
         gyro = new SimGyro();
         modules = new SimSwerveModule[] {
                 new SimSwerveModule(),
@@ -65,8 +80,12 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
                 SC.Swerve.brModuleOffset);
         odometry = new SwerveDriveOdometry(kinematics, gyro.getRotation2d(), getPositions());
 
+        double driveBaseRadius = 0;
+        for (var moduleLocation : m_moduleLocations) {
+            driveBaseRadius = Math.max(driveBaseRadius, moduleLocation.getNorm());
+        }
         // Configure AutoBuilder
-        AutoBuilder.configureHolonomic(
+        /*AutoBuilder.configureHolonomic(
                 this::getPose,
                 this::resetPose,
                 this::getSpeeds,
@@ -85,11 +104,28 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
                     return false;
                 },
                 this);
+*/
+ AutoBuilder.configureHolonomic(
+            ()->this.getState().Pose, // Supplier of current robot pose
+            this::seedFieldRelative,  // Consumer for seeding pose against auto
+            this::getCurrentRobotChassisSpeeds,
+            (speeds)->this.setControl(autoRequest.withSpeeds(speeds)), // Consumer of ChassisSpeeds to drive the robot
+            new HolonomicPathFollowerConfig(new PIDConstants(10, 0, 0),
+                                            new PIDConstants(10, 0, 0),
+                                            Swerve.kSpeedAt12VoltsMps,
+                                            driveBaseRadius,
+                                            new ReplanningConfig()),
+            ()->false, // Change this if the path needs to be flipped on red vs blue
+            this); // Subsystem for requirements
+    
 
         // Set up custom logging to add the current path to a field 2d widget
         PathPlannerLogging.setLogActivePathCallback((poses) -> field.getObject("path").setPoses(poses));
 
         SmartDashboard.putData("Field", field);
+    }
+    public ChassisSpeeds getCurrentRobotChassisSpeeds() {
+        return m_kinematics.toChassisSpeeds(getState().ModuleStates);
     }
 
     public Pose2d getPose() {
@@ -164,13 +200,13 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
         }
     }
 
-  public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
-    return new RunCommand(
-        () -> {
-          this.setControl(requestSupplier.get());
-        },
-        this);
-  }
+    public Command applyRequest(Supplier<SwerveRequest> requestSupplier) {
+        return new RunCommand(
+            () -> {
+            this.setControl(requestSupplier.get());
+            },
+            this);
+    }
   /**
      * Basic simulation of a gyro, will just hold its current state and not use any
      * hardware
@@ -186,4 +222,9 @@ public class Drivetrain extends SwerveDrivetrain implements Subsystem {
           currentRotation = currentRotation.plus(new Rotation2d(angularVelRps * 0.02));
       }
   }
-}
+    //public void periodic(){
+       // double yaw;
+       // yaw = pidgey.getAngle();
+       // SmartDashboard.putNumber("YawinDegrees", yaw);
+    }
+
